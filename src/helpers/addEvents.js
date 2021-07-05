d99d9a274b429ec74df5273231fd3af4265665e7
Dave Speake <dave@barnand.co>
2021-07-05 10:44:17 +0100

    Passive option.

--- src/helpers/addEvents.js
+++ src/helpers/addEvents.js
@@ -2,7 +2,8 @@ import { passiveOption } from './passiveOption.js';
 
 export function addEvents(el, obj, preventScrolling) {
   for (var prop in obj) {
-    var option = ['touchstart', 'touchmove'].indexOf(prop) >= 0 && !preventScrolling ? passiveOption : false;
+    //var option = ['touchstart', 'touchmove'].indexOf(prop) >= 0 && !preventScrolling ? passiveOption : false;
+    var option = passiveOption;
     el.addEventListener(prop, obj[prop], option);
   }
 }
